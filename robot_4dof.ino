65617962e4bd7c7a50153dc09db98b7c7e7885f3
Frank Blankenburg <frankcieslok@gmail.com>
2018-11-29 17:16:31 +0100

    Pressing a joystick button moved the robot to its home position. Structural improvements.

--- robot_4dof.ino
+++ robot_4dof.ino
@@ -26,11 +26,24 @@
 
 #include <Servo.h>
 
+//#*********************************************************************************
+// Configuration
+//#*********************************************************************************
+
+/*
+ * General configuration
+ */
+namespace Configuration
+{
+  // Servo step width
+  static const int step_width = 5;
+}
+
 /*
  * Port definitions
  */
-struct Port
-{
+namespace Port
+{  
   // Digital (PWM) output ports
   static const int servo_base = 5;
   static const int servo_arm1 = 9;
@@ -43,7 +56,16 @@ struct Port
 
   static const int joystick_2_h = A3;
   static const int joystick_2_v = A2;
+
+  // Digital inputs
+  static const int button_1 = 13;
+  static const int button_2 = 12;
 };
+
+
+//#*********************************************************************************
+// CLASS Axis
+//#*********************************************************************************
 
 /*
  * This class keeps the position of a single axis and updates it differentially
@@ -52,69 +74,78 @@ struct Port
 class Axis
 {
   public:
-    Axis (const char* name);
+    Axis (const char* name, int joystick_port, int servo_port, int initial_pos, int min_pos, int max_pos);
 
-    void setup (int joystick_port, int servo_port, int initial_value);
+    void setup ();
+    void reset ();
     void process ();
 
   private:
     const char* _name;
     Servo _servo;
     int _joystick_port;
+    int _servo_port;
+    int _initial_pos;
     int _middle_pos;
     int _current_pos;
+    int _min_pos;
+    int _max_pos;
 };
 
 /*
  * Constuctor
  * 
- * @param name Printable axis name (for debugging purposes)
+ * @param name          Printable axis name (for debugging purposes)
+ * @param joystick_port Analog input port which controls the axis
+ * @param servo_port    Digital (PWM) port the servo is connected to
+ * @param initial_pos   Initial servo position value in degree (0, 180)
  */
-Axis::Axis (const char* name)
+Axis::Axis (const char* name, int joystick_port, int servo_port, int initial_pos, int min_pos, int max_pos)
 : _name          (name),
   _servo         (),
-  _joystick_port (-1),
+  _joystick_port (joystick_port),
+  _servo_port    (servo_port),
+  _initial_pos   (initial_pos),
   _middle_pos    (1024 / 2),
-  _current_pos   (90)
+  _current_pos   (initial_pos),
+  _min_pos       (min_pos),
+  _max_pos       (max_pos)
 {
 }
 
 /*
- * Setup axis during system setup
- * 
- * @param joystick_port Analog input port which controls the axis
- * @param servo_port    Digital (PWM) port the servo is connected to
- * @param initial_value Initial servo position value in degree (0, 180)
+ * Setup axis
  */
-void Axis::setup (int joystick_port, int servo_port, int initial_value)
+void Axis::setup ()
 {
-  _joystick_port = joystick_port;
-  _current_pos = initial_value;
+  pinMode (_joystick_port, INPUT);
+  pinMode (_servo_port, OUTPUT);
   
-  pinMode (joystick_port, INPUT);
-  pinMode (servo_port, OUTPUT);
-  
-  _servo.attach (servo_port);  
+  _servo.attach (_servo_port);  
 
   // Calibrate by declaring the current joystick position as null position
-  _middle_pos = analogRead (joystick_port);
+  _middle_pos = analogRead (_joystick_port);
 }
+
+/*
+ * Reset axis to home position
+ */
+ void Axis::reset ()
+ {
+   _current_pos = _initial_pos;
+   _servo.write (_current_pos);
+ }
 
 /*
  * Process single step
  */
 void Axis::process ()
 {
-  // Map analog joystick value (0, 1024) into the range (-5, +5). That is the step width
+  // Map analog joystick value (0, 1024) into the range (-step_width, +step_width). That is the step width
   // in degree to be added to the current servo position.
-  int step = (_middle_pos - analogRead (_joystick_port)) / 100;
+  int step = map (_middle_pos - analogRead (_joystick_port), -512, 512, -Configuration::step_width, +Configuration::step_width);
   _current_pos += step;
-
-  // Usually, servos are expecting the position to be in the range of 0 to 180 degree.
-  if (_current_pos < 0)
-    _current_pos = 0;
-  if (_current_pos > 180)
-    _current_pos = 180;
+  _current_pos = constrain (_current_pos, _min_pos, _max_pos);
 
   // Print some debugging information
   Serial.print (_name);
@@ -125,25 +156,67 @@ void Axis::process ()
   Serial.print (step);
   Serial.print ("/P ");
   Serial.print (_current_pos);
+  Serial.print ("---");
   
   _servo.write (_current_pos);
 }
 
 
-Axis base ("Base");
-Axis arm1 ("Arm1");
-Axis arm2 ("Arm2");
-Axis hand ("Hand");
+//#*********************************************************************************
+// CLASS Button
+//#*********************************************************************************
 
+class Button
+{
+  public:
+    Button (int port) : _port (port), _state (false) {}
+
+    void setup ();
+    bool is_triggered ();
+
+  private:
+    int _port;
+    bool _state;
+};
+
+
+void Button::setup ()
+{
+  pinMode (_port, INPUT_PULLUP);  
+}
+
+bool Button::is_triggered ()
+{
+  bool state = digitalRead (_port) == LOW;
+  bool triggered = state && !_state;
+  _state = state;
+  return triggered;
+}
+
+
+//#*********************************************************************************
+// MAIN
+//#*********************************************************************************
+
+// Axis setup. The initial axis values are heuristics which may differ from robot
+// to robot.
+Axis base ("Base", Port::joystick_2_h, Port::servo_base,  90, 70, 140);
+Axis arm1 ("Arm1", Port::joystick_1_h, Port::servo_arm1,  20,  0,  90);
+Axis arm2 ("Arm2", Port::joystick_1_v, Port::servo_arm2,  90,  0, 180);
+Axis hand ("Hand", Port::joystick_2_v, Port::servo_hand, 100, 80, 130);
+
+Button button (Port::button_2);
+
+Axis* axes[] = {&base, &arm1, &arm2, &hand, nullptr};
 
 void setup ()
 {
-  // Axis setup. The initial axis values are heuristics which may differ from robot
-  // to robot.
-  base.setup (Port::joystick_2_h, Port::servo_base, 90);
-  arm1.setup (Port::joystick_1_h, Port::servo_arm1, 20);
-  arm2.setup (Port::joystick_1_v, Port::servo_arm2, 90);
-  hand.setup (Port::joystick_2_v, Port::servo_hand, 100);
+  for (int i=0; axes[i] != nullptr; ++i)
+    axes[i]->setup ();
+
+  // Digital input setup
+  pinMode (Port::button_1, INPUT_PULLUP);
+  pinMode (Port::button_2, INPUT_PULLUP);
 
   Serial.begin (9600);
 }
@@ -151,18 +224,23 @@ void setup ()
 
 void loop ()
 {
-  // Loop and adapt all axes. We are using C++11 here (just because we can...).
-  Axis* axis[] = {&base, &arm1, &arm2, &hand, nullptr};
-
-  for (int i=0; axis[i] != nullptr; ++i)
+  if (button.is_triggered ())
   {
-    Axis* a = axis[i];
-    a->process ();
-
-    Serial.print ("---");
-
-    delay (15);
+    Serial.println ("* HOME *");
+    
+    for (int i=0; axes[i] != nullptr; ++i)
+      axes[i]->reset ();
   }
 
+  // Loop and adapt all axes
+  for (int i=0; axes[i] != nullptr; ++i)
+    axes[i]->process ();
+
+  Serial.print (digitalRead (Port::button_1) == HIGH ? "high" : "low");
+  Serial.print ("---");
+  Serial.print (digitalRead (Port::button_2) == HIGH ? "high" : "low");
+  Serial.print ("---");
+
   Serial.println ();
+  delay (15);
 }
